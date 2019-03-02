f4b821e7b749cadea6be905f7418f010d1c18545
brettc-84 <brett.coomer@gmail.com>
2019-03-02 15:25:33 +0200

    changed some text

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ class App extends Component {
           >
             Learn React Now or Later
           </a>
+          <p>Changed on feature branch!</p>
         </header>
       </div>
     );
